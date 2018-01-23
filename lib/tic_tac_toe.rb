24ee961b86f1a35dbae10cba0dbdfd67ad46c210
Piyavadee Pariyavuth <piyavadeepariyavuth@gmail.com>
2018-01-23 02:32:09 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -90,7 +90,7 @@ class TicTacToe
       turn
     end
     if won?
-      puts "Congratulations #{winner(@board)}!"
+      puts "Congratulations #{winner}!"
     else
       puts "Cat's Game!"
     end
