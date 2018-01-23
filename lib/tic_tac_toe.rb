3ca252e0e6090fb89a5dbfa862032f0a85229142
Piyavadee Pariyavuth <piyavadeepariyavuth@gmail.com>
2018-01-23 02:06:08 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -25,8 +25,8 @@ class TicTacToe
   def move (index, token = "X")
     @board[index] = token
   end
-  def position_taken?(board, index)
+  def position_taken?(@board, index)
     @board[index] == "X" || @board[index] == "O"
-    
+
   end
 end
