b8a4d595368af659312ded549b9de8272aa5856a
Piyavadee Pariyavuth <piyavadeepariyavuth@gmail.com>
2018-01-23 01:48:43 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -20,6 +20,7 @@ class TicTacToe
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
   def input_to_index(input)
-    input.to_i - 1
+    @index = input.to_i - 1
   end
+  def move
 end
