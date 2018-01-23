736b78d0fc62c8a738d8d172bcebd3c85c30e8f9
Piyavadee Pariyavuth <piyavadeepariyavuth@gmail.com>
2018-01-23 02:10:36 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -31,4 +31,16 @@ class TicTacToe
   def valid_move?(index)
     index.between?(0,8) && !position_taken?(index)
   end
+  def turn
+ +  puts "Please enter 1-9:"
+ +  input = gets.strip
+ +  index = input_to_index(input)
+ +  if valid_move?(index)
+ +    player = current_player
+ +    move(index, token)
+ +    display_board
+ +  else
+ +    turn
+ +  end
+ +end
 end
