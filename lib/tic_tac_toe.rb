0006db8702f763b2760c73af618c37c55ac89c15
Piyavadee Pariyavuth <piyavadeepariyavuth@gmail.com>
2018-01-23 02:08:49 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -27,6 +27,7 @@ class TicTacToe
   end
   def position_taken?(index)
     @board[index] == "X" || @board[index] == "O"
-
   end
+  def valid_move?(index)
+    index.between?(0,8) && !position_taken?(index)
 end
